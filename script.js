1bcd040af9e68188fdd5f21e0f4a30cb1fe5c35f
Abdur Rakib <158442092+abdurrakib129@users.noreply.github.com>
2025-05-01 01:35:07 +0600

    Update script.js

--- script.js
+++ script.js
@@ -51,7 +51,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 /*=========================== Typed JS ==============================*/
 const typed = new Typed ('.multiple-text', {
-    strings: ['Web Automation Engineer', 'dApp Developer', 'Crypto Enthusiast'],
+    strings: ['Automation Engineer', 'dApp Developer', 'Crypto Enthusiast'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
